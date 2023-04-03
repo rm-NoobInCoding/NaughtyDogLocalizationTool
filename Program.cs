5ffb9cb6d88ab46bf64471d61f4be9a7de967e12
rezamms <srzrezamms@gmail.com>
2023-04-03 11:45:37 +0330

    Add import for ver 0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Be.IO;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace NaughtyDogLocalizationTool
     internal class Program
     {
         public static string[] LocalizationExtensions = { ".subtitles", ".common", ".subtitles-systemic" };
-        public static string UnknownString = "UNKNOWN STRING!!!";
+        public static byte[] UnknownString = Encoding.UTF8.GetBytes("UNKNOWN STRING!!!\0");
         static void Main(string[] args)
         {
 
@@ -18,103 +19,216 @@ namespace NaughtyDogLocalizationTool
                 Console.WriteLine("Naughty Dog Localization Tool");
                 Console.WriteLine("By NoobInCoding");
                 Console.WriteLine("Using this tool, you can edit the localization files of all Naughty Dog games (which contain the extensions .subtitles and .common).");
-                Console.WriteLine("For this, just drag the localization file into the tool to convert it into a text file");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\tExport: " + AppDomain.CurrentDomain.FriendlyName + " <localization file> [version (default is 2)]");
+                Console.WriteLine("");
                 Console.WriteLine("To convert it into a localization file, just drag the text file into the tool to create a new localization file");
+                Console.WriteLine("\tImport: " + AppDomain.CurrentDomain.FriendlyName + " <localization file>");
+                
                 Console.ReadKey();
             }
             else
             {
+
                 Console.WriteLine("Naughty Dog Localization Tool");
                 Console.WriteLine("By NoobInCoding");
                 if (LocalizationExtensions.Contains(Path.GetExtension(args[0])))
                 {
-                    BinaryReader Read = new BinaryReader(new MemoryStream(File.ReadAllBytes(args[0])));
-                    int ver = CheckVersion(File.ReadAllBytes(args[0]));
-                    List<long> offsets = new List<long>();
-                    List<string> ids = new List<string>();
-                    List<string> strings = new List<string>();
-                    ids.Add(ver + "|" + Path.GetExtension(args[0]).Replace(".", ""));
-                    int CountOfStrings = Read.ReadInt32();
-                    for (int i = 0; i < CountOfStrings; i++)
+                    int ver = 2;
+                    if (args.Length == 2) ver = Int32.Parse(args[1]);
+                    Console.WriteLine("Selected Ver" + ver);
+                    switch (ver)
                     {
-                        if (ver == 0)
-                        {
-                            ids.Add(Read.ReadUInt32().ToString());
-                            offsets.Add(Read.ReadInt32());
-                        }
-                        if (ver == 1)
-                        {
-                            ids.Add(Read.ReadUInt64().ToString());
-                            offsets.Add(Read.ReadInt64());
-                        }
-
+                        case 0:
+                            Export0(args[0]);
+                            break;
+                        case 1:
+                            Export1(args[0]);
+                            break;
+                        case 2:
+                            Export2(args[0]);
+                            break;
                     }
-                    long StringTablePos = Read.BaseStream.Position;
-                    for (int i = 0; i < CountOfStrings; i++)
-                    {
-                        Read.BaseStream.Position = StringTablePos + offsets[i];
-                        string str = Read.ReadNullTerminatedString();
-                        strings.Add(str);
-
-                    }
-                    File.WriteAllLines(args[0] + ".txt", strings);
-                    File.WriteAllLines(args[0] + ".ids", ids);
-                    Console.WriteLine("Done!");
 
                 }
                 else if (Path.GetExtension(args[0]) == ".txt")
                 {
-                    BinaryWriter Write = new BinaryWriter(new MemoryStream());
+
                     List<string> ids = new List<string>(File.ReadAllLines(Path.ChangeExtension(args[0], ".ids")));
                     List<string> strings = new List<string>(File.ReadAllLines(args[0]));
-                    List<byte[]> stringsInbyte = new List<byte[]>();
-                    string FileExtension = ids[0].Split('|')[1];
-                    int ver = Convert.ToInt32(ids[0].Split('|')[0]);
-                    ids.RemoveAt(0);
-                    int CountOfStrings = strings.Count;
-                    Write.Write(CountOfStrings);
-                    int TempOffsetv0 = UnknownString.Length;
-                    long TempOffsetv1 = 0;
-                    for (int i = 0; i < CountOfStrings; i++)
-                    {
-                        stringsInbyte.Add(Helpers.WriteNullTerminatedString(strings[i]));
-                        if (ver == 0)
-                        {
-                            Write.Write(Convert.ToUInt32(ids[i]));
-                            Write.Write(TempOffsetv0);
-                            TempOffsetv0 += stringsInbyte[i].Length;
-                        }
-                        else
-                        {
-                            Write.Write(Convert.ToUInt64(ids[i]));
-                            Write.Write(TempOffsetv1);
-                            TempOffsetv1 += stringsInbyte[i].Length;
-                        }
 
-                    }
-                    if (ver == 0) Write.Write(Encoding.UTF8.GetBytes(UnknownString + "\0"));
-                    for (int i = 0; i < CountOfStrings; i++)
+                    int ver = Convert.ToInt32(ids[0].Split('|')[0]);
+
+                    switch (ver)
                     {
-                        Write.Write(stringsInbyte[i]);
+                        case 2:
+                            Import2(args[0], ids, strings);
+                            break;
+                        case 1:
+                            Import1(args[0], ids, strings);
+                            break;
+                        case 0:
+                            Import0(args[0], ids, strings);
+                            break;
                     }
-                    File.WriteAllBytes(Path.ChangeExtension(args[0], FileExtension) + ".new", Write.ToByteArray());
-                    Console.WriteLine("Done!");
                 }
             }
         }
-        static int CheckVersion(byte[] Data)
+
+        static void Export2(string path)
         {
-            string tmp = Encoding.UTF8.GetString(Data);
-            if (tmp.Contains(UnknownString))
+            BinaryReader Read = new BinaryReader(new MemoryStream(File.ReadAllBytes(path)));
+            List<long> offsets = new List<long>();
+            List<string> ids = new List<string>();
+            List<string> strings = new List<string>();
+            ids.Add(3 + "|" + Path.GetExtension(path).Replace(".", ""));
+            int CountOfStrings = Read.ReadInt32();
+            for (int i = 0; i < CountOfStrings; i++)
             {
-                //TLOU 1 (PS4) - Uncharted (4 or newer)
-                return 0;
+                ids.Add(Read.ReadUInt64().ToString());
+                offsets.Add(Read.ReadInt64());
+
             }
-            else
+            long StringTablePos = Read.BaseStream.Position;
+            for (int i = 0; i < CountOfStrings; i++)
             {
-                //TLOU 2 - TLOU (PC)
-                return 1;
+                Read.BaseStream.Position = StringTablePos + offsets[i];
+                string str = Read.ReadNullTerminatedString();
+                strings.Add(str);
+
             }
+            File.WriteAllLines(path + ".txt", strings);
+            File.WriteAllLines(path + ".ids", ids);
+            Console.WriteLine("Done!");
+        }
+        static void Export1(string path)
+        {
+            BinaryReader Read = new BinaryReader(new MemoryStream(File.ReadAllBytes(path)));
+            List<int> offsets = new List<int>();
+            List<string> ids = new List<string>();
+            List<string> strings = new List<string>();
+            ids.Add(path + "|" + Path.GetExtension(path).Replace(".", ""));
+            int CountOfStrings = Read.ReadInt32();
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                ids.Add(Read.ReadUInt32().ToString());
+                offsets.Add(Read.ReadInt32());
+
+            }
+            long StringTablePos = Read.BaseStream.Position;
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                Read.BaseStream.Position = StringTablePos + offsets[i];
+                string str = Read.ReadNullTerminatedString();
+                strings.Add(str);
+
+            }
+            File.WriteAllLines(path + ".txt", strings);
+            File.WriteAllLines(path + ".ids", ids);
+            Console.WriteLine("Done!");
+        }
+        static void Export0(string path)
+        {
+            BeBinaryReader Read = new BeBinaryReader(new MemoryStream(File.ReadAllBytes(path)));
+            List<int> offsets = new List<int>();
+            List<string> ids = new List<string>();
+            List<string> strings = new List<string>();
+            ids.Add(path + "|" + Path.GetExtension(path).Replace(".", ""));
+            int CountOfStrings = Read.ReadInt32();
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                ids.Add(Read.ReadUInt32().ToString());
+                offsets.Add(Read.ReadInt32());
+
+            }
+            long baseoff = Read.BaseStream.Position;
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                Read.BaseStream.Seek(offsets[i], SeekOrigin.Begin);
+                string str = Read.ReadNullTerminatedString();
+                strings.Add(str);
+                Read.BaseStream.Seek(baseoff, SeekOrigin.Begin);
+
+            }
+            File.WriteAllLines(path + ".txt", strings);
+            File.WriteAllLines(path + ".ids", ids);
+            Console.WriteLine("Done!");
+        }
+
+        static void Import2(string txtpath, List<string> ids, List<string> strings)
+        {
+            BinaryWriter Write = new BinaryWriter(new MemoryStream());
+            List<byte[]> stringsInbyte = new List<byte[]>();
+            string FileExtension = ids[0].Split('|')[1];
+            ids.RemoveAt(0);
+            int CountOfStrings = strings.Count;
+            Write.Write(CountOfStrings);
+            long TempOffset = 0;
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                stringsInbyte.Add(Helpers.WriteNullTerminatedString(strings[i]));
+                Write.Write(Convert.ToUInt64(ids[i]));
+                Write.Write(TempOffset);
+                TempOffset += stringsInbyte[i].Length;
+
+            }
+            Write.Write(UnknownString);
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                Write.Write(stringsInbyte[i]);
+            }
+            File.WriteAllBytes(Path.ChangeExtension(txtpath, FileExtension) + ".new", Write.ToByteArray());
+            Console.WriteLine("Done!");
+        }
+        static void Import1(string txtpath, List<string> ids, List<string> strings)
+        {
+            BinaryWriter Write = new BinaryWriter(new MemoryStream());
+            List<byte[]> stringsInbyte = new List<byte[]>();
+            string FileExtension = ids[0].Split('|')[1];
+            ids.RemoveAt(0);
+            int CountOfStrings = strings.Count;
+            Write.Write(CountOfStrings);
+            long TempOffset = UnknownString.Length;
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                stringsInbyte.Add(Helpers.WriteNullTerminatedString(strings[i]));
+                Write.Write(Convert.ToUInt32(ids[i]));
+                Write.Write(TempOffset);
+                TempOffset += stringsInbyte[i].Length;
+
+            }
+            Write.Write(UnknownString);
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                Write.Write(stringsInbyte[i]);
+            }
+            File.WriteAllBytes(Path.ChangeExtension(txtpath, FileExtension) + ".new", Write.ToByteArray());
+            Console.WriteLine("Done!");
+        }
+        static void Import0(string txtpath, List<string> ids, List<string> strings)
+        {
+            BeBinaryWriter Write = new BeBinaryWriter(new MemoryStream());
+            List<byte[]> stringsInbyte = new List<byte[]>();
+            string FileExtension = ids[0].Split('|')[1];
+            ids.RemoveAt(0);
+            int CountOfStrings = strings.Count;
+            Write.Write(CountOfStrings);
+            long TempOffset = CountOfStrings * 8 + 4;
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                stringsInbyte.Add(Helpers.WriteNullTerminatedString(strings[i]));
+                Write.Write(Convert.ToUInt32(ids[i]));
+                Write.Write(TempOffset);
+                TempOffset += stringsInbyte[i].Length;
+
+            }
+            Write.Write(UnknownString);
+            for (int i = 0; i < CountOfStrings; i++)
+            {
+                Write.Write(stringsInbyte[i]);
+            }
+            File.WriteAllBytes(Path.ChangeExtension(txtpath, FileExtension) + ".new", Write.ToByteArray());
+            Console.WriteLine("Done!");
         }
 
     }
